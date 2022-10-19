cd856d0907633869c6443dba75df2ff2eb5148d1
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-19 14:01:01 +0200

    Inuppg22 lagt till test för isActiveMember i Customer

---
+++ Test/InUppg/Uppg22/CustomerTest.java
@@ -0,0 +1,24 @@
+package InUppg.Uppg22;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CustomerTest {
+    String mockName = "Ture Mackapär";
+    String mockID ="1603151234";
+    String mockDateValid = "2022-03-15";
+    String mockDateExpired = "2018-06-08";
+
+    Customer c1 = new Customer(mockID,mockName,mockDateValid);
+    Customer c2 = new Customer(mockID,mockName,mockDateExpired);
+
+    @Test
+    public void isActiveMemberTest(){ //test skrivet före äldre version av kod (som sedan ändrats något)
+        assertTrue (c1.isActiveMember());
+        assertFalse(c2.isActiveMember());
+        assertTrue(!c2.isActiveMember());
+    }
+
+}
