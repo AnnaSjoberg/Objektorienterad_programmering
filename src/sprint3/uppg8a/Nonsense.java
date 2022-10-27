552c58dd8cbde8462a0ad848b8cd42172a81bb2b
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-27 11:31:21 +0200

    här ska jag randomisera

--- src/sprint3/uppg8a/Nonsense.java
+++ src/sprint3/uppg8a/Nonsense.java
@@ -1,7 +1,5 @@
 package sprint3.uppg8a;
 
 public class Nonsense {
-
-    //kan det börja fungera nu?
-    //jag som trodde det här skulle vara en enkla delen
+//här testar jag sen att skapa 1c med randomiserat bildval
 }
