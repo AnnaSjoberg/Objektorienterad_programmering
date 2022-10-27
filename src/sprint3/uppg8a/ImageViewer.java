2305d8ce8bf1c6b0b0c34e76ca123f81ea08d017
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-27 11:29:37 +0200

    kopierat in uppg1

---
+++ src/sprint3/uppg8a/ImageViewer.java
@@ -0,0 +1,31 @@
+package sprint3.uppg8a;
+
+import sprint3.uppg1.OurActionListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageViewer extends JFrame {
+
+    JLabel image = new JLabel(new ImageIcon("/Users/HP/OneDrive - Nackademin AB/OOP/bilder/z1.jpg"));
+    JButton button = new JButton("Show next picture");
+    public ImageViewer(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,1));
+        add(panel);
+        button.addActionListener(new OurActionListener(image));
+        panel.add(image);
+        panel.add(button);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        ImageViewer iv = new ImageViewer();
+    }
+}
---
+++ src/sprint3/uppg8a/OurActionListener.java
@@ -0,0 +1,17 @@
+package sprint3.uppg8a;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OurActionListener implements ActionListener {
+    JLabel label;
+    public OurActionListener(JLabel label){
+        this.label=label;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        label.setIcon(new ImageIcon("/Users/HP/OneDrive - Nackademin AB/OOP/bilder/z2.jpg"));
+    }
+}
