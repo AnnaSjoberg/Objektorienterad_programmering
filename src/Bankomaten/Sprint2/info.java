e2abf209a9515ac2e2c4721b79e4461c43ee6e60
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-28 16:27:57 +0200

    lagt in uppgiftsbeskrivningen

---
+++ src/Bankomaten/Sprint2/info.java
@@ -0,0 +1,19 @@
+package Bankomaten.Sprint2;
+
+public class info {
+
+    /*
+    Bankomaten
+Utöka datamodellen till ditt banksystem. Om det inte redan finns, lägg till datum för när varje konto
+skapades och när varje lån utfärdades. Alla ändringar behöver också ha datum.
+Skapa filer som har datat för dina kunder, personal, konton och lån.
+Gör ett console-program som vänder sig till bankens kunder. De ska få se en meny som de sedan kan
+få välja ifrån. De ska kunna se kontoöversikt, eller ta ut pengar. Väljer de kontoöversikt ska de få se
+saldo på alla sina konton. Väljer de att ta ut pengar ska de få en fråga om hur mycket de ska ta ut.
+Sedan ska detta belopp dras av på saldot.
+Om du får tid över, gör ett alternativ ”utförlig kontoöversikt” där kunderna kan se ränta på varje konto
+samt även sina lån med ränta och nästa förfallodatum.
+Gör även ett admin-program för bankens personal. Där ska det gå att lista alla kunder, ändra
+kunduppgifter och ändra räntesats på konton och lån.
+     */
+}
