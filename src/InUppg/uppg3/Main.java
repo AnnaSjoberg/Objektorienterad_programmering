8647b9dc24a356ad2e6a369e90591caa4b432d3d
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-31 12:09:33 +0100

    ändrat lite i buttonsarray

--- src/InUppg/uppg3/Main.java
+++ src/InUppg/uppg3/Main.java
@@ -8,28 +8,32 @@ public class Main extends JFrame {
     JButton newGame = new JButton("New game");
 
 
-    JButton b1 = new JButton("1");
-    JButton b2 = new JButton("2");
-    JButton b3 = new JButton("3");
-    JButton b4 = new JButton("4");
-    JButton b5 = new JButton("5");
-    JButton b6 = new JButton("6");
-    JButton b7 = new JButton("7");
-    JButton b8 = new JButton("8");
-    JButton b9 = new JButton("9");
-    JButton b10 = new JButton("10");
-    JButton b11 = new JButton("11");
-    JButton b12 = new JButton("12");
-    JButton b13 = new JButton("13");
-    JButton b14 = new JButton("14");
-    JButton b15 = new JButton("15");
-    JButton b16 = new JButton(" ");
-    JButton[][] buttonsarray = {{b1,b2,b3,b4},{b5,b6,b7,b8},{b9,b10,b11,b12},{b13,b14,b15,b16}};
-
+ JButton[][] buttonsarray = {{
+         new JButton("1"),
+         new JButton("2"),
+         new JButton("3"),
+         new JButton("4")},
+         {new JButton("5"),
+                 new JButton("6"),
+                 new JButton("7"),
+                 new JButton("8")},
+         {new JButton("9"),
+                 new JButton("10"),
+                 new JButton("11"),
+                 new JButton("12")},
+         {new JButton("13"),
+                 new JButton("14"),
+                 new JButton("15"),
+                 new JButton(" ")}};
     //kommentar
     public Main() {
         PanelBuilder panelBuilder = new PanelBuilder();
         JPanel gamePanel = panelBuilder.gamePanel(buttonsarray);
+        add(gamePanel);
+
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 
