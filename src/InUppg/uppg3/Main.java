a77a5b5b2cba85a852bee798ab6612b133aa8e85
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-31 11:58:44 +0100

    test

--- src/InUppg/uppg3/Main.java
+++ src/InUppg/uppg3/Main.java
@@ -26,7 +26,7 @@ public class Main extends JFrame {
     JButton b16 = new JButton(" ");
     JButton[][] buttonsarray = {{b1,b2,b3,b4},{b5,b6,b7,b8},{b9,b10,b11,b12},{b13,b14,b15,b16}};
 
-
+    //kommentar
     public Main() {
         PanelBuilder panelBuilder = new PanelBuilder();
         JPanel gamePanel = panelBuilder.gamePanel(buttonsarray);
