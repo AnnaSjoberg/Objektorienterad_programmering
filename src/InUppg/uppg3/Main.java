44b6ad4551b7d1c5ad93fafd6abe7cad750ef578
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-31 16:26:03 +0100

    fixat fungerande randomiseringsmetod.

--- src/InUppg/uppg3/Main.java
+++ src/InUppg/uppg3/Main.java
@@ -2,50 +2,53 @@ package InUppg.uppg3;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main extends JFrame {
-// kommentar
+    // kommentar
     JLabel message = new JLabel();
     JButton newGame = new JButton("New game");
 
-
-     JButton[][] buttonsarray = {{
-         new JButton("1"),
-         new JButton("2"),
-         new JButton("3"),
-         new JButton("4")},
-         {new JButton("5"),
-                 new JButton("6"),
-                 new JButton("7"),
-                 new JButton("8")},
-         {new JButton("9"),
-                 new JButton("10"),
-                 new JButton("11"),
-                 new JButton("12")},
-         {new JButton("13"),
-                 new JButton("14"),
-                 new JButton("15"),
-                 new JButton("")}};
-
-     List<JButton> buttonsList = List.of(); //alternativt sätt att skapa upp alla knapparna.
+    //alternativt sätt att skapa upp alla knapparna.
     // Funderar på om detta är enklare för att kunna randomisera ordningen på knapparna när man ska starta nytt spel
+
+    List<JButton> buttonsList = List.of(
+            new JButton("1"),
+            new JButton("2"),
+            new JButton("3"),
+            new JButton("4"),
+            new JButton("5"),
+            new JButton("6"),
+            new JButton("7"),
+            new JButton("8"),
+            new JButton("9"),
+            new JButton("10"),
+            new JButton("11"),
+            new JButton("12"),
+            new JButton("13"),
+            new JButton("14"),
+            new JButton("15"),
+            new JButton(""));
+
 
     public Main() {
         setLayout(new BorderLayout());
         gameLogic g = new gameLogic();
-
-        List<String> list = List.of("1","2,","3","4","5","6","7","8"
-                ,"9","10","11","12","13","14","15","");
-        g.randomizeListV(list);
-        System.out.println(list.get(0) +" "+ list.get(5));
+        List<String> list = g.randomizeList();
+    //    List<String> list = List.of("1", "2,", "3", "4", "5", "6", "7", "8"
+       //         , "9", "10", "11", "12", "13", "14", "15", "");
+   //     g.randomizeListV(list);
+    //    System.out.println(list.get(0) + " " + list.get(5));
 
         PanelBuilder panelBuilder = new PanelBuilder();
-        JPanel gamePanel = panelBuilder.gamePanel(buttonsarray);
-        add(gamePanel, BorderLayout.SOUTH);
+       // JPanel gamePanel = panelBuilder.gamePanel(buttonsarray);
+        //add(gamePanel, BorderLayout.SOUTH);
 
-        add(newGame,BorderLayout.WEST);
-        add(message,BorderLayout.EAST);
+        add(newGame, BorderLayout.WEST);
+        add(message, BorderLayout.EAST);
+
 
 
 
--- src/InUppg/uppg3/gameLogic.java
+++ src/InUppg/uppg3/gameLogic.java
@@ -9,30 +9,28 @@ import java.util.List;
 
 public class gameLogic extends MouseAdapter {
     List<JButton> sortedButtonList = new ArrayList<>(); //om denna -> använda konstruktor för att sätta värdena
-    protected final List<String> sortedStringList = List.of("1","2,","3","4","5","6","7","8"
-                                                                ,"9","10","11","12","13","14","15","");
+
 
     public List<String> getSortedStringList() {
+        List<String> sortedStringList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            sortedStringList.add(""+(i+1));
+        }
         return sortedStringList;
     }
 
     public boolean isSorted (List<String> inputList){
         for (int i = 0; i < inputList.size(); i++) {
-            if (inputList.get(i) != sortedStringList.get(i)){
+            if (inputList.get(i) != getSortedStringList().get(i)){
                 return false;
             }
         }
         return true;
     }
     public List<String> randomizeList (){
-        List<String> rList = getSortedStringList();
+        List<String> rList =  getSortedStringList();
          Collections.shuffle(rList);
-        System.out.println(rList.size());
          return rList;
-    }
-
-    public void randomizeListV (List<String> input){
-        Collections.shuffle(input);
     }
 
     // en mouseClicked med lambdas för Nytt spel som randomiserar alla knappar
