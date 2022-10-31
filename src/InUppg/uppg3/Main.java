a57a6b59a2c28eb45ea10cd9c7563851ef6f215c
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2022-10-31 11:32:36 +0100

    börjat på panelerna

--- src/InUppg/uppg3/Main.java
+++ src/InUppg/uppg3/Main.java
@@ -1,8 +1,31 @@
 package InUppg.uppg3;
 
-public class Main {
+import javax.swing.*;
 
-    //mainprogram för spelet
-    //test med en ny kommentar
+public class Main extends JFrame {
 
+    JLabel message = new JLabel();
+    JButton newGame = new JButton("New game");
+
+    JButton b1 = new JButton("1");
+    JButton b2 = new JButton("2");
+    JButton b3 = new JButton("3");
+    JButton b4 = new JButton("4");
+    JButton b5 = new JButton("5");
+    JButton b6 = new JButton("6");
+    JButton b7 = new JButton("7");
+    JButton b8 = new JButton("8");
+    JButton b9 = new JButton("9");
+    JButton b10 = new JButton("10");
+    JButton b11 = new JButton("11");
+    JButton b12 = new JButton("12");
+    JButton b13 = new JButton("13");
+    JButton b14 = new JButton("14");
+    JButton b15 = new JButton("15");
+    JButton b16 = new JButton(" ");
+
+
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
 }
---
+++ src/InUppg/uppg3/PanelBuilder.java
@@ -0,0 +1,30 @@
+package InUppg.uppg3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelBuilder {
+
+    public JPanel gamePanel (JButton[][] buttons){
+        JPanel res = new JPanel(new GridLayout(4,4));
+
+        res.add(buttons[0][0]);
+        res.add(buttons[0][1]);
+        res.add(buttons[0][2]);
+        res.add(buttons[0][3]);
+        res.add(buttons[1][0]);
+        res.add(buttons[1][1]);
+        res.add(buttons[1][2]);
+        res.add(buttons[1][3]);
+        res.add(buttons[2][0]);
+        res.add(buttons[2][1]);
+        res.add(buttons[2][2]);
+        res.add(buttons[2][3]);
+        res.add(buttons[3][0]);
+        res.add(buttons[3][1]);
+        res.add(buttons[3][2]);
+        res.add(buttons[3][3]);
+
+
+    }
+}
